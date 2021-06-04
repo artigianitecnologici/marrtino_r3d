203911cd57bf89c890e6399f04f23ad92a5078ea
Fabio Ferrarini <ferrarini09@gmail.com>
2021-06-04 04:42:02 +0200

    Update install_other.sh

--- install/install_other.sh
+++ install/install_other.sh
@@ -1,12 +1,12 @@
 cd $HOME/src
-git clone https://github.com/artigianitecnologici/speech.git
+git clone https://github.com/artigianitecnologici/talk.git
 git clone https://github.com/artigianitecnologici/ros_waypoint_generator.git
 git clone https://github.com/artigianitecnologici/marrtinox_description.git
 git clone https://github.com/artigianitecnologici/marrwy_description.git
 
 # Link
 cd $HOME/ros/catkin_ws/src
-      ln -s $HOME/src/speech .
+      ln -s $HOME/src/talk .
       ln -s $HOME/src/ros_waypoint_generator .
       ln -s $HOME/src/marrtinox_description .
       ln -s $HOME/src/marrwy_description .
