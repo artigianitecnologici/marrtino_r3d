04194e5684d9a822925d4357014ad909e5297751
Fabio Ferrarini <ferrarini09@gmail.com>
2021-07-29 12:03:48 +0200

    Update install_srrg.sh
    Aggiornamento installazione

--- install/install_srrg.sh
+++ install/install_srrg.sh
@@ -2,7 +2,17 @@
 # ROS Melodic, navigation, perception & additional packages
 # Version 4 - 9/8/2020
 
-
+sudo apt update
+sudo apt install libwebsockets-dev
+mkdir -p $HOME/src/srrg
+cd $HOME/src/srrg
+git clone https://gitlab.com/srrg-software/srrg_cmake_modules.git
+git clone https://gitlab.com/srrg-software/srrg2_orazio.git 
+cd $HOME/ros/catkin_ws/src
+ln -s $HOME/src/srrg/srrg_cmake_modules .
+ln -s $HOME/src/srrg/srrg2_orazio .
+cd $HOME/ros/catkin_ws
+catkin_make -j1
 
 
 # MARRtino ROS node
