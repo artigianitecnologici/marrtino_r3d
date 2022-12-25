388fa631c6b06103398431ffe9825b9d1319bb7d
Fabio Ferrarini <ferrarini09@gmail.com>
2022-12-25 10:20:40 +0000

    script shell

---
+++ script/garage.sh
@@ -0,0 +1,3 @@
+python ./movexyabs.py START 1 -0.5 0 garage 
+python ./movexyabs.py GO 2 0.5 0 garage 
+python ./movexyabs.py GO 3 -0.5 0 garage 
---
+++ sh/make_orazio.sh
@@ -0,0 +1,4 @@
+cd $HOME/src/srrg/srrg2_orazio/srrg2_orazio/firmware_build/atmega2560  
+make clean
+make
+make orazio.hex
